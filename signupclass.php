6b8c60384c70320db577366d358a6da5e0f47b0f
WMacharia <grace.macharia@strathmore.edu>
2024-09-27 11:51:33 +0300

    class done signup form

---
+++ signupclass.php
@@ -0,0 +1,9 @@
+<?php
+    require_once "load.php";
+    $objlayouts->heading();
+    $objmenus->main_menu();
+    //$objContents->main_content();
+    $ObjLayouts->banner();
+    $Objforms->sign_up_form();
+    $objContents->sidebar();
+    $objlayouts->footer();
