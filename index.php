851f93b4d4fcdf94391ad4d2a52d990af1137271
WMacharia <grace.macharia@strathmore.edu>
2024-09-27 11:52:19 +0300

    index page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     require_once "load.php";
     $objlayouts->heading();
     $objmenus->main_menu();
+    $Objlayouts->banner();
     $objContents->main_content();
     $objContents->sidebar();
     $objlayouts->footer();
